e2dde200dba06016039722e6b329f2ab629ca5f2
Bryce Griner <bryce.griner@pearson.com>
2022-08-19 13:35:59 -0700

    Intellij Spring Init - Creating simple Spring Boot web application.

---
+++ src/main/java/ai/prama/javas3demo/JavaS3DemoApplication.java
@@ -0,0 +1,13 @@
+package ai.prama.javas3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaS3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaS3DemoApplication.class, args);
+    }
+
+}
